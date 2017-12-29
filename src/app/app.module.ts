3b210d1641c67b296a8dd0cda03ce572acc6b103
Malcolm Gourdine <malcolmgourdine@gmail.com>
2017-12-29 00:11:44 -0500

    styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TracklistComponent } from './tracklist/tracklist.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TracklistComponent
   ],
   imports: [
     BrowserModule
