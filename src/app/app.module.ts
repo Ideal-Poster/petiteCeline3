d71970db1978c214d8e5db4205b6da2d9dde0175
Malcolm Gourdine <malcolmgourdine@gmail.com>
2018-01-07 00:22:13 -0500

    refactoring

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component'
+import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { SplashPageComponent } from './splash-page/splash-page.component';
+import { EventsPageComponent } from './events-page/events-page.component'
 ;
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    SplashPageComponent,
+    EventsPageComponent
   ],
   imports: [
     BrowserModule
